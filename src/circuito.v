5c63b5949dad7a1815f961e3cb2ee10e47d2ab12
Angelo <cupertinoangelo13@gmail.com>
2022-03-16 19:50:34 -0300

    :guitar: feat: Adicionar logica

--- src/circuito.v
+++ src/circuito.v
@@ -1,32 +1,77 @@
-module Circuito (a, b, c, d, e, f, g, tb_b5, tb_b4, tb_b3, tb_b2, tb_b1);
-    reg  [6:0] resultado;
-    output a, b, c, d, e, f, g;
-    input tb_b5, tb_b4, tb_b3, tb_b2, tb_b1;
-    always @(*)
-    begin
-        case({tb_b1, tb_b2, tb_b3, tb_b4, tb_b5})
-            5'b00000: resultado = 7'b0001110; //L
-            5'b00001: resultado = 7'b1011111; //6
-            5'b00010: resultado = 7'b0011100; //V
-            5'b00011: resultado = 7'b1100111; //P
-            5'b00100: resultado = 7'b0111100; //J
-            5'b00101: resultado = 7'b0000101; //R
-            5'b00110: resultado = 7'b1110000; //7
-            5'b00111: resultado = 7'b1101101; //2
-            5'b01000: resultado = 7'b0111110; //U
-            5'b01001: resultado = 7'b0011111; //B
-            5'b01010: resultado = 7'b1101101; //Z
-            5'b01011: resultado = 7'b0110011; //4
-            5'b01100: resultado = 7'b1111111; //8
-            5'b01101: resultado = 7'b0101010; //W
-            5'b01110: resultado = 7'b1001111; //E
-            5'b01111: resultado = 7'b1111110; //0
-            5'b10000: resultado = 7'b1111001; //3
-            5'b10001: resultado = 7'b1110111; //A
-            5'b10010: resultado = 7'b1111110; //O
-            5'b10011: resultado = 7'b1100000; //1
-            default: resultado = 7'b0000000;
-        endcase
+module Circuito (a, b, c, d, reset, tb_b8, tb_b7, tb_b6, tb_b5, tb_b4, tb_b3, tb_b2, tb_b1);
+
+    reg [3:0] estadoAtual = 4'b0000;
+    reg finalizado = 1'b0;
+    reg [6:0] entrada;
+    output a, b, c, d;
+    input reset, tb_b8, tb_b7, tb_b6, tb_b5, tb_b4, tb_b3, tb_b2, tb_b1;
+
+    always @(*) begin
+
+        entrada = {tb_b7, tb_b6, tb_b5, tb_b4, tb_b3, tb_b2, tb_b1};
+
+        if (reset == 1'b1) begin
+            estadoAtual = 4'b0000;
+            finalizado = 1'b0;
+        end
+        if (tb_b8 == 1'b1 && finalizado != 1'b1) begin
+            if ( entrada == 7'b1011000) begin
+                if (estadoAtual == 4'b0000 || estadoAtual == 4'b0010) begin // for igual a 0 ou 2
+                    estadoAtual = 4'b0001; //1
+                end else begin
+                    estadoAtual = 4'b1001; //7
+                    finalizado = 1'b1;
+                end
+            end
+            if ( entrada == 7'b1101011) begin
+                if (estadoAtual == 4'b0000 || estadoAtual == 4'b0001 || estadoAtual == 4'b0011) begin // for igual a 0 ou 1 ou 3
+                    estadoAtual = 4'b0010; //2
+                end else begin
+                    estadoAtual = 4'b1001; //7
+                    finalizado = 1'b1;
+                end
+            end
+            if ( entrada == 7'b1001111) begin
+                if (estadoAtual == 4'b0000 || estadoAtual == 4'b0010 || estadoAtual == 4'b0100) begin // for igual a 0 ou 2 ou 4
+                    estadoAtual = 4'b0011; //3
+                end else begin
+                    estadoAtual = 4'b1001; //7
+                    finalizado = 1'b1;
+                end
+            end
+            if ( entrada == 7'b0101000) begin
+                if (estadoAtual == 4'b0000 || estadoAtual == 4'b0011 || estadoAtual == 4'b0101) begin // for igual a 0 ou 3 ou 5
+                    estadoAtual = 4'b0100; //4
+                end else begin
+                    estadoAtual = 4'b1001; //7
+                    finalizado = 1'b1;
+                end
+            end
+            if ( entrada == 7'b0001100) begin
+                if (estadoAtual == 4'b0000 || estadoAtual == 4'b0100) begin // for igual a 0 ou 4
+                    estadoAtual = 4'b0101; //5
+                end else begin
+                    estadoAtual = 4'b1001; //7
+                    finalizado = 1'b1;
+                end
+            end
+            if ( entrada == 7'b0110010) begin
+                if (estadoAtual == 4'b0001 || estadoAtual == 4'b0010 || estadoAtual == 4'b0011) begin // for igual 1 ou 2 ou 3
+                    estadoAtual = 4'b1000; //6
+                    finalizado = 1'b1;
+                end
+            end
+            if ( entrada == 7'b0010110) begin
+                estadoAtual = 4'b1001; //7
+                finalizado = 1'b1;
+            end
+            if ( entrada == 7'b0100011 && estadoAtual != 4'b0000) begin
+                if (estadoAtual == 4'b0100 || estadoAtual == 4'b0101) begin // for igual 4 ou 5
+                    estadoAtual = 4'b1010; //8
+                    finalizado = 1'b1;
+                end
+            end
+        end
     end
-    assign {a, b, c, d, e, f, g} = resultado;
+    assign {a, b, c, d} = estadoAtual;
 endmodule
--- src/circuito_tb.v
+++ src/circuito_tb.v
@@ -1,43 +1,21 @@
 `include "circuito.v"
 
 module values_TB;
-    wire a, b, c, d, e, f, g;
-    reg tb_b5, tb_b4, tb_b3, tb_b2, tb_b1;
-    Circuito dut( a, b, c, d, e, f, g, tb_b5, tb_b4, tb_b3, tb_b2, tb_b1);
+    wire a, b, c, d;
+    reg reset, tb_b8, tb_b7, tb_b6, tb_b5, tb_b4, tb_b3, tb_b2, tb_b1;
+    Circuito dut( a, b, c, d, reset, tb_b8, tb_b7, tb_b6, tb_b5, tb_b4, tb_b3, tb_b2, tb_b1);
     initial begin
-        $dumpvars(0, values_TB);
-        $monitor("entrada = %b%b%b%b%b | saida = %b%b%b%b%b%b%b", tb_b1, tb_b2, tb_b3, tb_b4, tb_b5,  a, b, c, d, e, f, g);
-        tb_b5 = 0;  tb_b4 = 0; tb_b3 = 0; tb_b2 = 0; tb_b1 = 0; #1
-        tb_b5 = 1;  tb_b4 = 0; tb_b3 = 0; tb_b2 = 0; tb_b1 = 0; #1
-        tb_b5 = 0;  tb_b4 = 1; tb_b3 = 0; tb_b2 = 0; tb_b1 = 0; #1
-        tb_b5 = 1;  tb_b4 = 1; tb_b3 = 0; tb_b2 = 0; tb_b1 = 0; #1
-        tb_b5 = 0;  tb_b4 = 0; tb_b3 = 1; tb_b2 = 0; tb_b1 = 0; #1
-        tb_b5 = 1;  tb_b4 = 0; tb_b3 = 1; tb_b2 = 0; tb_b1 = 0; #1
-        tb_b5 = 0;  tb_b4 = 1; tb_b3 = 1; tb_b2 = 0; tb_b1 = 0; #1
-        tb_b5 = 1;  tb_b4 = 1; tb_b3 = 1; tb_b2 = 0; tb_b1 = 0; #1
-        tb_b5 = 0;  tb_b4 = 0; tb_b3 = 0; tb_b2 = 1; tb_b1 = 0; #1
-        tb_b5 = 1;  tb_b4 = 0; tb_b3 = 0; tb_b2 = 1; tb_b1 = 0; #1
-        tb_b5 = 0;  tb_b4 = 1; tb_b3 = 0; tb_b2 = 1; tb_b1 = 0; #1
-        tb_b5 = 1;  tb_b4 = 1; tb_b3 = 0; tb_b2 = 1; tb_b1 = 0; #1
-        tb_b5 = 0;  tb_b4 = 0; tb_b3 = 1; tb_b2 = 1; tb_b1 = 0; #1
-        tb_b5 = 1;  tb_b4 = 0; tb_b3 = 1; tb_b2 = 1; tb_b1 = 0; #1
-        tb_b5 = 0;  tb_b4 = 1; tb_b3 = 1; tb_b2 = 1; tb_b1 = 0; #1
-        tb_b5 = 1;  tb_b4 = 1; tb_b3 = 1; tb_b2 = 1; tb_b1 = 0; #1
-        tb_b5 = 0;  tb_b4 = 0; tb_b3 = 0; tb_b2 = 0; tb_b1 = 1; #1
-        tb_b5 = 1;  tb_b4 = 0; tb_b3 = 0; tb_b2 = 0; tb_b1 = 1; #1
-        tb_b5 = 0;  tb_b4 = 1; tb_b3 = 0; tb_b2 = 0; tb_b1 = 1; #1
-        tb_b5 = 1;  tb_b4 = 1; tb_b3 = 0; tb_b2 = 0; tb_b1 = 1; #1
-        tb_b5 = 0;  tb_b4 = 0; tb_b3 = 1; tb_b2 = 0; tb_b1 = 1; #1
-        tb_b5 = 1;  tb_b4 = 0; tb_b3 = 1; tb_b2 = 0; tb_b1 = 1; #1
-        tb_b5 = 0;  tb_b4 = 1; tb_b3 = 1; tb_b2 = 0; tb_b1 = 1; #1
-        tb_b5 = 1;  tb_b4 = 1; tb_b3 = 1; tb_b2 = 0; tb_b1 = 1; #1
-        tb_b5 = 0;  tb_b4 = 0; tb_b3 = 0; tb_b2 = 1; tb_b1 = 1; #1
-        tb_b5 = 1;  tb_b4 = 0; tb_b3 = 0; tb_b2 = 1; tb_b1 = 1; #1
-        tb_b5 = 0;  tb_b4 = 1; tb_b3 = 0; tb_b2 = 1; tb_b1 = 1; #1
-        tb_b5 = 1;  tb_b4 = 1; tb_b3 = 0; tb_b2 = 1; tb_b1 = 1; #1
-        tb_b5 = 0;  tb_b4 = 0; tb_b3 = 1; tb_b2 = 1; tb_b1 = 1; #1
-        tb_b5 = 1;  tb_b4 = 0; tb_b3 = 1; tb_b2 = 1; tb_b1 = 1; #1
-        tb_b5 = 0;  tb_b4 = 1; tb_b3 = 1; tb_b2 = 1; tb_b1 = 1; #1
-        tb_b5 = 1;  tb_b4 = 1; tb_b3 = 1; tb_b2 = 1; tb_b1 = 1; #1;
+        $monitor("Entrada = %b%b%b%b%b%b%b%b | Estado Atual = %b%b%b%b", tb_b8, tb_b7, tb_b6, tb_b5, tb_b4, tb_b3, tb_b2, tb_b1, a, b, c, d);
+        reset = 0; tb_b8 = 1;  tb_b7 = 1; tb_b6 = 0; tb_b5 = 1;  tb_b4 = 1; tb_b3 = 0; tb_b2 = 0; tb_b1 = 0; #1
+        reset = 0; tb_b8 = 1;  tb_b7 = 1; tb_b6 = 1; tb_b5 = 0;  tb_b4 = 1; tb_b3 = 0; tb_b2 = 1; tb_b1 = 1; #1
+        reset = 0; tb_b8 = 1;  tb_b7 = 1; tb_b6 = 0; tb_b5 = 0;  tb_b4 = 1; tb_b3 = 1; tb_b2 = 1; tb_b1 = 1; #1
+        reset = 0; tb_b8 = 1;  tb_b7 = 0; tb_b6 = 1; tb_b5 = 0;  tb_b4 = 1; tb_b3 = 0; tb_b2 = 0; tb_b1 = 0; #1
+        reset = 0; tb_b8 = 1;  tb_b7 = 0; tb_b6 = 0; tb_b5 = 0;  tb_b4 = 1; tb_b3 = 1; tb_b2 = 0; tb_b1 = 0; #1
+        reset = 0; tb_b8 = 1;  tb_b7 = 0; tb_b6 = 1; tb_b5 = 1;  tb_b4 = 0; tb_b3 = 0; tb_b2 = 1; tb_b1 = 0; #1
+        reset = 0; tb_b8 = 1;  tb_b7 = 0; tb_b6 = 0; tb_b5 = 1;  tb_b4 = 0; tb_b3 = 1; tb_b2 = 1; tb_b1 = 0; #1
+        reset = 0; tb_b8 = 1;  tb_b7 = 0; tb_b6 = 1; tb_b5 = 0;  tb_b4 = 0; tb_b3 = 0; tb_b2 = 1; tb_b1 = 1; #1;
+
+        reset = 1; tb_b8 = 0;  tb_b7 = 0; tb_b6 = 1; tb_b5 = 0;  tb_b4 = 0; tb_b3 = 0; tb_b2 = 1; tb_b1 = 1; #1;
+        reset = 0; tb_b8 = 0;  tb_b7 = 0; tb_b6 = 1; tb_b5 = 0;  tb_b4 = 0; tb_b3 = 0; tb_b2 = 1; tb_b1 = 0; #1;
     end
 endmodule
